eb4ecd7f38c3edc0c27a99ff5676f9e039080db0
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-06-01 19:42:45 +0600

    Nav bar completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Nav from "./Nav/Nav";
+import { ThemeProvider } from "@/components/theme-provider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav></Nav>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </ThemeProvider>
+);
