3435438bf799d7abcba0fdc9286d4cf4e17de6e8
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-06-03 18:02:39 +0600

    Terms & Condition Completed

--- src/Terms&Condition/Terms.jsx
+++ src/Terms&Condition/Terms.jsx
@@ -1,33 +1,54 @@
 const Terms = () => {
   return (
-    <div>
+    <div className="my-10 lg:my-16 max-w-7xl mx-auto">
       {" "}
-      <h1>Terms and Conditions</h1>
-      <h2>Introduction</h2>
-      <p>
-        Welcome to [Your Website Name] (“we”, “our”, “us”). These Terms and
-        Conditions (“Terms”) govern your use of our website and services. By
-        accessing or using our website, you agree to be bound by these Terms. If
-        you do not agree with any part of these Terms, please do not use our
-        website.
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center">
+        Terms and Conditions
+      </h1>
+      <h2 className="my-6 lg:my-8 text-center text-xl md:text-2xl lg:text-3xl font-bold">
+        Introduction
+      </h2>
+      <p className="font-semibold my-2">
+        Welcome to PetConnect. These Terms and Conditions govern your use of our
+        website and services. By accessing or using our website, you agree to be
+        bound by these Terms. If you do not agree with any part of these Terms,
+        please do not use our website.
       </p>
-      <h3>1. Definitions</h3>
-      <div>User: Any person who accesses and uses the website.</div>
-      <div>Adopter: A user who adopts a pet through our website.</div>
-      <div>Pet: Any animal listed for adoption on our website.</div>
+      <h3 className="md:text-xl lg:text-2xl font-semibold my-2">
+        1. Definitions
+      </h3>
       <div>
-        Service: The pet adoption services and any other services provided by us
-        through the website.
+        <span className="font-semibold">User:</span> Any person who accesses and
+        uses the website.
       </div>
-      <h3>2. Eligibility</h3>
+      <div>
+        <span className="font-semibold">Adopter: </span> A user who adopts a pet
+        through our website.
+      </div>
+      <div>
+        <span className="font-semibold">Pet:</span> Any animal listed for
+        adoption on our website.
+      </div>
+      <div>
+        <span className="font-semibold"> Service:</span>
+        The pet adoption services and any other services provided by us through
+        the website.
+      </div>
+      <h3 className=" md:text-xl lg:text-2xl font-semibold my-2">
+        2. Eligibility
+      </h3>
       <div>
         {" "}
         To use our website, you must be at least 18 years old and legally
         capable of entering into binding contracts. By using our website, you
         represent and warrant that you meet these eligibility requirements.
       </div>
-      <h3>3. User Responsibilities</h3>
-      <h4>Account Registration</h4>
+      <h3 className="md:text-xl lg:text-2xl font-semibold my-2">
+        3. User Responsibilities
+      </h3>
+      <h4 className="text-bold mb-2 font-semibold lg:text-xl">
+        Account Registration
+      </h4>
       <div>
         You may need to create an account to access certain features of our
         website. You are responsible for maintaining the confidentiality of your
@@ -35,16 +56,24 @@ const Terms = () => {
         account. You agree to notify us immediately of any unauthorized use of
         your account.
       </div>
-      <h3>Accurate Information</h3>
+      <h3 className="md:text-xl lg:text-2xl font-semibold my-2">
+        Accurate Information
+      </h3>
       <div>
         You agree to provide accurate, current, and complete information during
         the registration process and to update such information to keep it
         accurate, current, and complete.
       </div>
-      <h3>Prohibited Activities</h3>
-      <ul>
-        You agree not to use the website for any unlawful or prohibited
-        activities, including but not limited to:
+      <h3 className="text-xs md:text-xl lg:text-2xl font-semibold my-2">
+        Prohibited Activities
+      </h3>
+      <ul className="list-disc ">
+        <div className="md:text-xl mb-2">
+          {" "}
+          You agree not to use the website for any unlawful or prohibited
+          activities, including but not limited to:
+        </div>
+
         <li>
           Impersonating any person or entity, or falsely stating or otherwise
           misrepresenting your affiliation with a person or entity.
@@ -62,39 +91,43 @@ const Terms = () => {
           Violating any applicable local, state, national, or international law.{" "}
         </li>
       </ul>
-      <h3>4. Adoption Process</h3>
-      <h4>Application</h4>
+      <h3 className="text-xs md:text-xl lg:text-2xl font-semibold my-2">
+        4. Adoption Process
+      </h3>
+      <h4 className="mt-2 mb-1 text-xl font-semibold">Application:</h4>
       <div>
         To adopt a pet, you must complete an adoption application. The
         application process may include an interview, home visit, and other
         checks as deemed necessary by us.
       </div>
-      <h4>Approval</h4>
+      <h4 className="mt-2 mb-1 text-xl font-semibold">Approval:</h4>
       <div>
         Approval of adoption applications is at our sole discretion. We reserve
         the right to refuse any application for any reason.
       </div>
-      <h4>Adoption Fees</h4>
+      <h4 className="mt-2 mb-1 text-xl font-semibold">Adoption Fees:</h4>
       <div>
         Adoption fees help cover the costs of caring for the pets. Fees are
         non-refundable and must be paid in full before the adoption is
         finalized.
       </div>
-      <h3>5. Intellectual Property</h3>
-      <h4>Ownership</h4>
+      <h3 className="md:text-xl lg:text-2xl font-semibold my-2">
+        5. Intellectual Property
+      </h3>
+      <h4 className="mt-2 mb-1 text-xl font-semibold">Ownership:</h4>
       <p>
         All content on the website, including but not limited to text, graphics,
         logos, images, and software, is the property of [Your Website Name] or
         its content suppliers and is protected by intellectual property laws.
       </p>
-      <h3>Limited License</h3>
+      <h3 className="mt-2 mb-1 text-xl font-semibold">Limited License:</h3>
       <p>
         We grant you a limited, non-exclusive, non-transferable, and revocable
         license to access and use the website for personal and non-commercial
         purposes. This license does not include any resale or commercial use of
         the website or its contents.
       </p>
-      <h3>Restrictions</h3>
+      <h3 className="mt-2 mb-1 text-xl font-semibold">Restrictions:</h3>
       <p>
         You may not reproduce, distribute, modify, create derivative works of,
         publicly display, publicly perform,
